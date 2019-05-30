7e4cabbcf6a51a0e90f7b1c20d0c4c2f0d8829c4
raziel <x-alexander93@yandex.ru>
2019-05-30 19:07:11 +0300

    add initial parseHelp() with tests

---
+++ test/argparse.test.ts
@@ -0,0 +1,44 @@
+describe('--help parser', () => {
+    it('should return an empty array if no args are found', () => {
+        expect(parseHelp('')).toEqual([])
+    });
+    it('should parse a single arg in a single line', () => {
+        const expected: Array<Arg> = [{shortArg:'-v', description: 'Print version'}];
+        expect(parseHelp('-v    Print version')).toEqual(expected);
+    });
+    it('should parse a single arg in multiple lines', () => {
+        const expected: Array<Arg> = [
+            {shortArg:'-v', description: 'Print version'},
+            {shortArg:'-g', description: '-g opt'},
+        ];
+        expect(parseHelp('-v    Print version\n\n-g  -g opt')).toEqual(expected);
+    });
+});
+
+
+function parseHelp(output: string): Array<Arg> {
+    let res: Array<Arg> = [];
+
+    for (let line of output.split("\n")) {
+        let splitted = /^(\S+)\s+(.+)/.exec(line);
+        if (splitted) {
+            res.push({shortArg: splitted[1], description: splitted[2]});
+        }
+    }
+
+    return res;
+}
+
+interface Arg {
+    description: string;
+    longArg?: string;
+    shortArg?: string;
+}
+
+// TODO CLI:
+// should print an error if no argument is specified
+
+// Parser:
+// should return an object with corresponding description and long and/or short option
+// should return a [] if no options are found
+
