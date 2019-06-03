d916516e9dc0773445662e8715938daa33c99089
raziel <raziell@ya.ru>
2019-06-03 17:27:05 +0300

    add finished parseArgLine() with tests

--- test/argparse.test.ts
+++ test/argparse.test.ts
@@ -16,11 +16,35 @@ describe('--help parser', () => {
 });
 
 describe('parseArgLine', () => {
-    it('should return a null on empty string', () => {
-        expect(parseArgLine('')).toEqual(null)
+    describe('should return a null', () => {
+        it('if string is empty', () => {
+            expect(parseArgLine('')).toEqual(null)
+        });
+        it('if string does not contain an arg', () => {
+            expect(parseArgLine('some irrelevant text')).toEqual(null)
+        });
     });
-    it('should return a null if string does not contain an arg', () => {
-        expect(parseArgLine('some irrelevant text')).toEqual(null)
+    describe('should return an arg', () => {
+        it('has string containing short arg and description', () => {
+            expect(parseArgLine('-s    a short arg')).toEqual({shortArg:'-s', description: 'a short arg'});
+        });
+        it('has string containing long arg and description', () => {
+            expect(parseArgLine('--long a long arg')).toEqual({longArg:'--long', description: 'a long arg'});
+        });
+        it('has string containing both short and long arg and description', () => {
+            expect(parseArgLine('-s, --long short and long arg')).toEqual({
+                shortArg:'-s',
+                longArg: '--long',
+                description: 'short and long arg'
+            });
+        });
+        it('has string containing both short and long arg and description with hyphen', () => {
+            expect(parseArgLine('-s, --long - 1 short and 1 long - the arg')).toEqual({
+                shortArg:'-s',
+                longArg: '--long',
+                description: '1 short and 1 long - the arg'
+            });
+        });
     });
 });
 
@@ -43,11 +67,34 @@ function parseArgLine(line: string): Arg | null {
         return null;
     }
 
-    let splitted = /^(\S+)\s+(.+)/.exec(line);
-    if (splitted && splitted[1] && splitted[2]) {
-        return {shortArg: splitted[1], description: splitted[2]};
+    const shortArg = hasArg(/(?<!-)-\w+/.exec(line));
+    const longArg = hasArg(/--\w+/.exec(line));
+    if (!Boolean(shortArg || longArg)) {
+        return null;
     }
+
+    const description = line
+        .replace(shortArg || '', '')
+        .replace(longArg || '', '')
+        .replace(/^\W+\s/, '').trim()
+    ;
+    if (description) {
+        let arg: Arg = {description: description};
+        if (shortArg) {
+            arg.shortArg = shortArg;
+        }
+        if (longArg) {
+            arg.longArg = longArg;
+        }
+        return arg;
+    }
+
     return null;
+
+    function hasArg(regexResult:  RegExpExecArray | null): string | null {
+        return regexResult && regexResult[0];
+    }
+
 }
 
 interface Arg {
